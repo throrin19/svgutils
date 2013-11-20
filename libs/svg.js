251d35b6e66db06c0dddd1e5b6f857363eebc3a8
Benjamin Besse <benjamin.besse02@gmail.com>
2013-11-20 12:16:44 +0100

    + add jsdoc

--- libs/svg.js
+++ libs/svg.js
@@ -10,14 +10,27 @@ var Svg = function(){};
 
 Svg.prototype.elements = [];
 
+/**
+ * Set Svg Elements
+ * @param {Array}       elements            SvgObject Array (rect|polygon|polyline|...)
+ */
 Svg.prototype.setElements = function(elements){
     this.elements = elements;
 };
 
+/**
+ * Add SvgObject element to current SVG
+ * @param {SvgObject}   element             SvgObject Element
+ */
 Svg.prototype.addElement = function(element){
     this.elements.push(element);
 };
 
+/**
+ * Convert Svg to Json format
+ * @param {boolean}     matrix              Applies Matrix and convert rect to polygon and circle to ellipse
+ * @returns {object}                        Svg Json Object representation
+ */
 Svg.prototype.toJSON = function(matrix){
     var json = {
         elements : []
@@ -30,6 +43,11 @@ Svg.prototype.toJSON = function(matrix){
     return json;
 };
 
+/**
+ * Convert Svg to Xml format
+ * @param {boolean}     matrix              Applies Matrix and convert rect to polygon and circle to ellipse
+ * @returns {object}                        XMLBuilder Svg representation
+ */
 Svg.prototype.toXml = function(matrix){
     var xml = builder.create('svg');
     xml.att('version', '1.1');
@@ -42,12 +60,23 @@ Svg.prototype.toXml = function(matrix){
     return xml;
 };
 
+/**
+ * Convert SVG to String :
+ *     '<svg>...</svg>'
+ * @param {boolean}     matrix              Applies Matrix and convert rect to polygon and circle to ellipse
+ * @returns {string}                        Svg String representation
+ */
 Svg.prototype.toString = function(matrix){
     return this.toXml(matrix).toString();
 };
 
 module.exports = Svg;
 
+/**
+ * Create Svg from Svg Document
+ * @param {string}      path                Uri of source file
+ * @param {function}    callback            Callback Function
+ */
 module.exports.fromSvgDocument = function(path, callback){
     fs.readFile(path, function(error, data){
         if(error){
@@ -58,6 +87,11 @@ module.exports.fromSvgDocument = function(path, callback){
     });
 };
 
+/**
+ * Create Svg from Xml String representation
+ * @param {string}      string              Svg string representation
+ * @param {function}    callback            Callback Function
+ */
 module.exports.fromString = function(string, callback){
     var parser  = new xml2js.Parser(),
         self    = this;
